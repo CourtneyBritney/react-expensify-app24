b9d24842fa622e6228db7745fbf3b964935044b9
CourtneyBritney <courtney02alexander@gmail.com>
2020-10-08 13:48:00 +0200

    Creating a Separate Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBap8AJklJ8tj_qS8bSebgu0aNBXA2mMUY",
-  authDomain: "expensify-f3539.firebaseapp.com",
-  databaseURL: "https://expensify-f3539.firebaseio.com",
-  projectId: "expensify-f3539",
-  storageBucket: "expensify-f3539.appspot.com",
-  messagingSenderId: "281051521931",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: "1:281051521931:web:5c41e25706b3e779e2058e",
   measurementId: "G-5GMK8MPJWJ"
 };
